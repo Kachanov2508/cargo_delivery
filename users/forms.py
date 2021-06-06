b77a0916c30e1391a26cd022f3880a01230c56d6
Kachanov2508 <kachanov2508@yandex.ru>
2021-06-06 18:13:42 +0300

    forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,19 @@
+from django import forms
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    email = forms.EmailField(label='Email',
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- users/views.py
+++ users/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
 
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Вы успешно зарегистрировались')
@@ -15,7 +17,7 @@ def register(request):
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
